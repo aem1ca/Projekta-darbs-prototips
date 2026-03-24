f58f441f2e64a71d3f2c58889e13a1f8ae2124ba
KarlisGP <karlis.plavenieki@gmail.com>
2026-03-24 20:16:23 +0200

    bug aniated

--- Assets/Scripts/PlayerControllerBored.cs
+++ Assets/Scripts/PlayerControllerBored.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerBored : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed = 5f;
